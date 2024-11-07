4500992fe0651ba5a1cfe7e2d66e22d8bffbb93b
colbyj427 <colbyj427@gmail.com>
2024-11-07 16:31:04 -0700

    added github to login page

--- src/login/logIn.jsx
+++ src/login/logIn.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './login.css';
+import { AuthState } from './authState';
 
 
-export function Login() {
+export function Login({ userName, authState, onAuthChange }) {
   return (
+
+    <main>
+      <div>
+        {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+      </div>
     <main className='container-fluid text-center'>
         <div className="login-container">
             <h2>Login</h2>
@@ -13,6 +30,10 @@ export function Login() {
                 <input type="submit" value="Login"/>
             </form>
         </div>
+        <div>
+            <a class="btn btn-warning" href="https://github.com/colbyj427/CS260-startup" role="button">Github</a>
+        </div>
+    </main>
     </main>
   );
 }
