2ffd1ff3fec28408af9c462a70be48aa29e03eb2
colbyj427 <colbyj427@gmail.com>
2024-11-07 20:39:06 -0700

    working authentication

--- src/login/logIn.jsx
+++ src/login/logIn.jsx
@@ -6,7 +6,7 @@ import { AuthState } from './authState';
 
 export function Login({ userName, authState, onAuthChange }) {
   return (
-    <main className='container-fluid bg-secondary text-center'>
+    <main className='container-fluid text-center'>
       <div>
         {authState !== AuthState.Unknown && <h1>Start Cooking!</h1>}
         {authState === AuthState.Authenticated && (
--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -1,3 +1,53 @@
+import React from 'react';
+
+import Button from 'react-bootstrap/Button';
+import { MessageDialog } from './messageDialog';
+
+export function Unauthenticated(props) {
+  const [userName, setUserName] = React.useState(props.userName);
+  const [password, setPassword] = React.useState('');
+  const [displayError, setDisplayError] = React.useState(null);
+
+  async function loginUser() {
+    localStorage.setItem('userName', userName);
+    props.onLogin(userName);
+  }
+
+  async function createUser() {
+    localStorage.setItem('userName', userName);
+    props.onLogin(userName);
+  }
+
+  return (
+    <>
+      <div>
+      <div className="login-container">
+        <h2>Login</h2>
+        <div className='input-group mb-3'>
+          <input className='form-control' type='text' value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='username' />
+        </div>
+        <div className='input-group mb-3'>
+          <input className='form-control' type='password' onChange={(e) => setPassword(e.target.value)} placeholder='password' />
+        </div>
+        <div class="submitButtons">
+        <Button variant='primary' onClick={() => loginUser()} disabled={!userName || !password}>
+          Login
+        </Button>
+        <Button variant='secondary' onClick={() => createUser()} disabled={!userName || !password}>
+          Create
+        </Button>
+        </div>
+      </div>
+      </div>
+      <div>
+      <a class="btn btn-warning" href="https://github.com/colbyj427/CS260-startup" role="button">Github</a>
+      </div>
+
+      <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
+    </>
+  );
+}
+
 // import React from 'react';
 // import './login.css';
 // import Button from 'react-bootstrap/Button';
@@ -49,54 +99,3 @@
 //     </>
 //   );
 // }
-
-
-import React from 'react';
-
-import Button from 'react-bootstrap/Button';
-import { MessageDialog } from './messageDialog';
-
-export function Unauthenticated(props) {
-  const [userName, setUserName] = React.useState(props.userName);
-  const [password, setPassword] = React.useState('');
-  const [displayError, setDisplayError] = React.useState(null);
-
-  async function loginUser() {
-    localStorage.setItem('userName', userName);
-    props.onLogin(userName);
-  }
-
-  async function createUser() {
-    localStorage.setItem('userName', userName);
-    props.onLogin(userName);
-  }
-
-  return (
-    <>
-      <div>
-      <div className="login-container">
-        <h2>Login</h2>
-        <div className='input-group mb-3'>
-          <input className='form-control' type='text' value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='your@email.com' />
-        </div>
-        <div className='input-group mb-3'>
-          <input className='form-control' type='password' onChange={(e) => setPassword(e.target.value)} placeholder='password' />
-        </div>
-        <div class="submitButtons">
-        <Button variant='primary' onClick={() => loginUser()} disabled={!userName || !password}>
-          Login
-        </Button>
-        <Button variant='secondary' onClick={() => createUser()} disabled={!userName || !password}>
-          Create
-        </Button>
-        </div>
-      </div>
-      </div>
-      <div>
-      <a class="btn btn-warning" href="https://github.com/colbyj427/CS260-startup" role="button">Github</a>
-      </div>
-
-      <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
-    </>
-  );
-}
